42ab0cf1ce1b516d6874c511f22956293d2573f2
emerson_acacio@lccv.ufal.br <emerson_acacio@lccv.ufal.br>
2022-03-03 10:55:14 -0300

    conclusão do middleware de autenticação

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
+const JWT_SECRET_KEY =
+  "ASASghdskjdbfjsdhbfjhehbueyuhuwwoijkokldfndvjdfhjuihgtçpewpijuoi";
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+function auth(req, res, next) {
+  const authToken = req.headers["authorization"];
+  if (authToken) {
+    const token = authToken.split(" ")[1];
+    jwt.verify(token, JWT_SECRET_KEY, (err, data) => {
+      if (err) {
+        res.status(400);
+        res.json({ errors: [{ message: "Token invalido" }] });
+      } else {
+        req.loggedUser = { ...data };
+        next();
+      }
+    });
+  } else {
+    res.status(401);
+    res.json({ errors: [{ message: "Token inválido" }] });
+  }
+}
 
 var DB = {
   games: [
@@ -12,15 +35,29 @@ var DB = {
     { id: 65, title: "aaaaa", year: 1834, price: 80 },
     { id: 2, title: "cccccc", year: 2021, price: 90 },
   ],
+  users: [
+    {
+      id: 1,
+      name: "Emerson",
+      email: "emerson@hotmail.com",
+      password: "123456",
+    },
+    {
+      id: 2,
+      name: "Acácio",
+      email: "acacio@hotmail.com",
+      password: "123456",
+    },
+  ],
 };
 var maxId = 100;
 
-app.get("/games", (req, res) => {
+app.get("/games", auth, (req, res) => {
   res.statusCode = 200;
-  res.json(DB.games);
+  res.json({ user: req.loggedUser, games: DB.games });
 });
 
-app.get("/game/:id", (req, res) => {
+app.get("/game/:id", auth, (req, res) => {
   const { id } = req.params;
   if (isNaN(id)) {
     res.sendStatus(400);
@@ -35,7 +72,7 @@ app.get("/game/:id", (req, res) => {
   }
 });
 
-app.post("/game", (req, res) => {
+app.post("/game", auth, (req, res) => {
   const { title, year, price } = req.body;
 
   maxId++;
@@ -45,7 +82,7 @@ app.post("/game", (req, res) => {
   res.sendStatus(200);
 });
 
-app.delete("/game/:id", (req, res) => {
+app.delete("/game/:id", auth, (req, res) => {
   let { id } = req.params;
   if (isNaN(id)) {
     res.sendStatus(400);
@@ -59,7 +96,7 @@ app.delete("/game/:id", (req, res) => {
   }
 });
 
-app.put("/game/:id", (req, res) => {
+app.put("/game/:id", auth, (req, res) => {
   let { id } = req.params;
   if (isNaN(id)) {
     res.sendStatus(400);
@@ -85,6 +122,43 @@ app.put("/game/:id", (req, res) => {
     } else {
       res.sendStatus(404);
     }
+  }
+});
+
+app.post("/auth", (req, res) => {
+  const { email, password } = req.body;
+
+  if (email && password) {
+    const user = DB.users.find((user) => user.email === email);
+    if (user) {
+      if (user.password === password) {
+        jwt.sign(
+          { id: user.id, email: user.email },
+          JWT_SECRET_KEY,
+          {
+            expiresIn: "4h",
+          },
+          (err, token) => {
+            if (err) {
+              res.status(400);
+              res.json({ errors: [{ message: "falha na geração do token" }] });
+            } else {
+              res.status(200);
+              res.json({ token });
+            }
+          }
+        );
+      } else {
+        res.status(401);
+        res.json({ errors: [{ message: "Senha inválida" }] });
+      }
+    } else {
+      res.status(404);
+      res.json({ errors: [{ message: "Usuário não existe" }] });
+    }
+  } else {
+    res.status(400);
+    res.json({ errors: [{ message: "e-mail ou(e) senha inválido(s)" }] });
   }
 });
 
